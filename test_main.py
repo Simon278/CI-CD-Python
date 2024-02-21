a227b38cd48fa4aa9cab4eb26618c2734868f591
Austrix007 <98654656+Austrix007@users.noreply.github.com>
2024-02-21 17:24:35 +0100

    initial commit

---
+++ test_main.py
@@ -0,0 +1,18 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(main.add(3, 4), 7)
+
+    def test_subtract(self):
+        self.assertEqual(main.subtract(5, 2), 3)
+
+    def test_multiply(self):
+        self.assertEqual(main.multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(main.divide(10, 2), 5)
+        with self.assertRaises(ValueError):
+            main.divide(10, 0)
